9c345f685d9a70afeda8f0ead2d107f6883b879c
Benjamin Inglis <bjammin134@gmail.com>
2025-06-04 10:00:15 -0700

    Add error component to UI

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import MealItem from "./MealItem.jsx";
+import Error from "./Error.jsx";
 import useHTTP from "../hooks/useHTTP.js";
 
 const requestConfig = {};
@@ -11,7 +12,11 @@ export default function Meals() {
   } = useHTTP("http://localhost:3000/meals", requestConfig, []);
 
   if (isLoading) {
-    return <p>Fetching meals...</p>;
+    return <p className="center">Fetching meals...</p>;
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals" message={error.message} />;
   }
 
   return (
