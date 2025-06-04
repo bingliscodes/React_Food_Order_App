3e1f7b69e2b9ca6d23dd5abac2edb8ef0570afdd
Benjamin Inglis <bjammin134@gmail.com>
2025-06-04 10:25:03 -0700

    Clear data upon order completion

--- src/hooks/useHTTP.js
+++ src/hooks/useHTTP.js
@@ -19,6 +19,10 @@ export default function useHTTP(url, config, initialData) {
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState(initialData);
 
+  function clearData() {
+    setData(initialData);
+  }
+
   const sendRequest = useCallback(
     async function sendRequest(data) {
       setIsLoading(true);
@@ -44,5 +48,6 @@ export default function useHTTP(url, config, initialData) {
     isLoading,
     error,
     sendRequest,
+    clearData,
   };
 }
